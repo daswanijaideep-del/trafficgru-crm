dbb9110ede83470c94a94a96ca4df3729845ea5a
daswanijaideep-del <daswanijaideep@gmail.com>
2026-06-27 20:51:45 +0530

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,31 +1,59 @@
 const generateToken = require("../utils/generateToken");
 
 const login = (req, res) => {
+
     const { username, password } = req.body;
 
     if (
         username !== process.env.ADMIN_USERNAME ||
         password !== process.env.ADMIN_PASSWORD
     ) {
+
         return res.status(401).json({
             success: false,
             message: "Invalid username or password"
         });
+
     }
 
     const token = generateToken();
 
-    res.json({
+    return res.status(200).json({
+
         success: true,
-        token
+
+        message: "Login successful",
+
+        data: {
+
+            user: {
+
+                username: process.env.ADMIN_USERNAME,
+
+                role: "Administrator"
+
+            },
+
+            token
+
+        }
+
     });
+
 };
 
 const me = (req, res) => {
-    res.json({
+
+    return res.status(200).json({
+
         success: true,
-        user: req.user
+
+        message: "User fetched successfully",
+
+        data: req.user
+
     });
+
 };
 
 module.exports = {
