df0087cc993d0c6502155a7b0274bf2355db1fb3
daswanijaideep-del <daswanijaideep@gmail.com>
2026-06-27 20:02:48 +0530

    Add files via upload

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -5,9 +5,16 @@ const router = express.Router();
 const authenticate = require("../middleware/auth.middleware");
 
 const {
-    getAllLeads
+    getAllLeads, getLeadById, updateLead,archiveLead
 } = require("../controllers/admin.controller");
 
 router.get("/leads", authenticate, getAllLeads);
+router.get("/leads/:id", authenticate, getLeadById);
+router.patch("/leads/:id", authenticate, updateLead);
+router.patch(
+    "/leads/:id/archive",
+    authenticate,
+    archiveLead
+);
 
 module.exports = router;
---
+++ routes/dashboard.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+const authenticate = require("../middleware/auth.middleware");
+
+const {
+    getDashboard
+} = require("../controllers/dashboard.controller");
+
+router.get("/", authenticate, getDashboard);
+
+module.exports = router;
