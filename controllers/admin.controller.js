df67d46872fd97524a5f3a50e4ed10b8030f6a9e
daswanijaideep-del <daswanijaideep@gmail.com>
2026-06-28 10:56:01 +0530

    Add files via upload

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -262,11 +262,146 @@ const archiveLead = async (req, res) => {
 
 };
 
+const restoreLead = async (req, res) => {
+
+    try {
+
+        const lead = await Lead.findByIdAndUpdate(
+
+            req.params.id,
+
+            {
+                isArchived: false
+            },
+
+            {
+                new: true
+            }
+
+        );
+
+        if (!lead) {
+
+            return res.status(404).json({
+
+                success: false,
+
+                message: "Lead not found"
+
+            });
+
+        }
+
+        res.status(200).json({
+
+            success: true,
+
+            message: "Lead restored successfully",
+
+            data: lead
+
+        });
+
+    }
+
+    catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+
+            success: false,
+
+            message: error.message
+
+        });
+
+    }
+
+};
+
+const getArchivedLeads = async (req, res) => {
+
+    try {
+
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const search = req.query.search || "";
+
+        const query = {
+            isArchived: true
+        };
+
+        if (search) {
+
+            query.$or = [
+
+                { leadNumber: { $regex: search, $options: "i" } },
+                { name: { $regex: search, $options: "i" } },
+                { company: { $regex: search, $options: "i" } },
+                { email: { $regex: search, $options: "i" } },
+                { phone: { $regex: search, $options: "i" } }
+
+            ];
+
+        }
+
+        const totalRecords = await Lead.countDocuments(query);
+
+        const totalPages = Math.ceil(totalRecords / limit);
+
+        const leads = await Lead.find(query)
+            .sort({ createdAt: -1 })
+            .skip((page - 1) * limit)
+            .limit(limit);
+
+        return res.json({
+
+            success: true,
+
+            message: "Archived leads fetched successfully",
+
+            data: {
+
+                leads,
+
+                pagination: {
+
+                    page,
+                    limit,
+                    totalPages,
+                    totalRecords
+
+                }
+
+            }
+
+        });
+
+    }
+
+    catch (error) {
+
+        console.error(error);
+
+        return res.status(500).json({
+
+            success: false,
+
+            message: error.message
+
+        });
+
+    }
+
+};
 module.exports = {
 
     getAllLeads,
     getLeadById,
     updateLead,
-    archiveLead
+    archiveLead,
+    restoreLead,
+    getArchivedLeads
 
 };
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -5,16 +5,29 @@ const router = express.Router();
 const authenticate = require("../middleware/auth.middleware");
 
 const {
-    getAllLeads, getLeadById, updateLead,archiveLead
+    getAllLeads, getLeadById, updateLead,archiveLead, restoreLead, getArchivedLeads
 } = require("../controllers/admin.controller");
 
 router.get("/leads", authenticate, getAllLeads);
+router.get(
+    "/leads/archived",
+    authenticate,
+    getArchivedLeads
+);
 router.get("/leads/:id", authenticate, getLeadById);
 router.patch("/leads/:id", authenticate, updateLead);
+
+
 router.patch(
     "/leads/:id/archive",
     authenticate,
     archiveLead
 );
+router.patch(
+    "/leads/:id/restore",
+    authenticate,
+    restoreLead
+);
+
 
 module.exports = router;
